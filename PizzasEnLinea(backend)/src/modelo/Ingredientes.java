a936085d643eeaf18d0a4cb5f5deff31e7844fb3
SamantaAlfaro <samantasoul2612@gmail.com>
2020-06-19 16:56:02 -0600

    pizza, ingredientes listo

---
+++ PizzasEnLinea(backend)/src/modelo/Ingredientes.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Samanta
+ */
+public class Ingredientes implements Serializable{
+    private double precio;
+    private String nombre;
+
+    public Ingredientes() {
+        this(0.0, null);
+    }
+
+    public Ingredientes(double precio, String nombre) {
+        this.precio = precio;
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredientes{" + "precio=" + precio + ", nombre=" + nombre + '}';
+    }   
+    
+}
--- PizzasEnLinea(backend)/src/modelo/Pizza.java
+++ PizzasEnLinea(backend)/src/modelo/Pizza.java
@@ -5,10 +5,67 @@
  */
 package modelo;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 /**
  *
  * @author Samanta
  */
-public class Pizza {
+public class Pizza implements Serializable{
+    private String tamaño;
+    private String nombre;
+    private double precio; //fijo
+    private ArrayList<String> ingredientes;
+
+    public Pizza() {
+        this(null,null,0.0, null);
+    }
+
+    public Pizza(String tamaño, String nombre, double precio, ArrayList<String> ingredientes) {
+        this.tamaño = tamaño;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.ingredientes = ingredientes;
+    }
+
+    public String getTamaño() {
+        return tamaño;
+    }
+
+    public void setTamaño(String tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public ArrayList<String> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(ArrayList<String> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    @Override
+    public String toString() {
+        return "Pizza{" + "tama\u00f1o=" + tamaño + ", nombre=" + nombre + ", precio=" + precio + ", ingredientes=\n" + ingredientes.toString() + '}';
+    }
+    
+    
     
 }
