c437906ee38deeaf9752c30ba01fd88b7e3b65cb
SamantaAlfaro <samantasoul2612@gmail.com>
2020-06-19 17:16:52 -0600

    new mongo db driver

--- PizzasEnLinea(backend)/src/DAO/GestorUsuarios.java
+++ PizzasEnLinea(backend)/src/DAO/GestorUsuarios.java
@@ -5,10 +5,46 @@
  */
 package DAO;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import java.io.Serializable;
+
 /**
  *
  * @author Samanta
  */
-public class GestorUsuarios {
+public class GestorUsuarios implements Serializable{
+    
+    private static GestorUsuarios instancia = null;//para singleton
+    private static MongoClient mongo = null;
+    private String baseDatos;
+    MongoDatabase db;
+    
+    private GestorUsuarios() {//para crear la conexion como un cliente de mongo
+        try {
+            mongo = (MongoClient) MongoClients.create("mongodb://localhost:27017");
+            crearConexion();
+        } catch (Exception ex) {
+            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+        }
+    }
+
+    private void crearConexion() {
+        baseDatos = "facturas";
+        db = mongo.getDatabase(baseDatos);
+        System.out.println("Conexión completada..");
+    }
+
+    public static GestorUsuarios getInstance() {//para no estar haciendo instancias a cada rato en memoria
+        if (instancia == null) {
+            instancia = new GestorUsuarios();
+        }
+        return instancia;
+    }
+    
+    
+    
+    
     
 }
