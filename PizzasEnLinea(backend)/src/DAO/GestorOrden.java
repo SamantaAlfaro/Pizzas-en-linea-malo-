17ac77addf146a5e0a6d451c1f0e413b844ea966
KalaPinaki <leobago15@gmail.com>
2020-06-19 21:04:32 -0600

    gestorOrden terminado

---
+++ PizzasEnLinea(backend)/src/DAO/GestorOrden.java
@@ -0,0 +1,104 @@
+package DAO;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import modelo.Pizza;
+import modelo.orden;
+import org.bson.Document;
+
+public class GestorOrden {
+
+    private static GestorOrden instancia = null;
+    private static MongoClient mongo = null;
+    private String baseDatos;
+    MongoDatabase db;
+
+    public GestorOrden() {
+        try {
+            mongo = MongoClients.create("mongodb://localhost:27017");
+            crearConexion();
+        } catch (Exception ex) {
+            System.err.printf("Excepción: '%s'%n", ex.getMessage());
+        }
+    }
+
+    private void crearConexion() {
+        baseDatos = "pizzeria";
+        db = mongo.getDatabase(baseDatos);
+        System.out.println("Conexión completada..");
+    }
+
+    public static GestorOrden getInstance() {
+        if (instancia == null) {
+            instancia = new GestorOrden();
+        }
+        return instancia;
+    }
+    
+        public void insertOrder(orden or) {
+        MongoCollection<Document> collection = db.getCollection("ordenes");
+
+        Document Order = new Document();
+        Order.append("ced cliente", or.getCedula()).
+                append("nomb cliente", or.getNombreCompleto()).
+                append("num orden", or.getNumOrden()).
+                append("fecha", or.getFecha()).
+                append("pizzas", or.getPizzas()).
+                append("extras", or.getExtras()).
+                append("estado", or.getEstado()).
+                append("metodo pago", or.getMetodo_pago()).
+                append("precio", or.getPrecio());
+
+        collection.insertOne(Order);
+    }
+
+    public List<Document> listOrder() {
+        List<Document> Orders = new ArrayList<Document>();
+        MongoCollection<Document> collection = db.getCollection("ordenes");
+
+        Orders = collection.find().projection(new Document()).into(new ArrayList<Document>());
+
+        return Orders;
+    }
+
+    
+    public Document getOrder(int num) {
+        List<Document> orders = new ArrayList<Document>();
+        Document order = new Document();
+
+        MongoCollection<Document> collection = db.getCollection("ordenes");
+        orders = collection.find().projection(new Document()).into(new ArrayList<Document>());
+
+        for (Document d : orders) {
+            if (d.get("num orden").equals(num)) {
+                return order = d;
+            }
+        }
+        return order;
+    }
+
+        public static void main(String[] args) {
+//        GestorOrden prueba = getInstance();
+//        ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
+//        ArrayList<String> extras = new ArrayList<String>();
+//
+//        orden u = new orden("304950273", "Leonardo Baldares Gómez", 1, pizzas, extras, new Date(), 0,"contado",20000);
+//        prueba.insertOrder(u);
+//        orden u2 = new orden("304980727", "Melissa Fernandez Ramirez", 2, pizzas, extras, new Date(), 0,"tarjeta",100000);
+//        prueba.insertOrder(u2);
+//        
+//        System.out.println(prueba.listOrder().toString());
+//        System.out.println(prueba.getOrder(1).toString());
+//        prueba.updateUser("304950273", "Heredia", "11111111");
+//        prueba.updatePassword("304950273", "leo123");
+    }
+    
+    
+
+}
---
+++ PizzasEnLinea(backend)/src/modelo/orden.java
@@ -0,0 +1,119 @@
+package modelo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class orden implements Serializable {
+
+    private String cedula;
+    private String nombreCompleto;
+    private int numOrden;
+    private ArrayList<Pizza> pizzas;
+    private ArrayList<String> extras;
+    private Date fecha;
+    private int estado;
+    private String metodo_pago;
+    private double precio;
+
+    public orden() {
+        this.cedula = "";
+        this.nombreCompleto = "";
+        this.numOrden = 0;
+        this.pizzas = new ArrayList<>();
+        this.extras = new ArrayList<>();
+        this.fecha = null;
+        this.estado = 0;
+        this.metodo_pago = "";
+        this.precio = 0.0;
+    }
+
+    public orden(String cedula, String nombreCompleto, int numOrden, ArrayList<Pizza> pizzas, ArrayList<String> extras, Date fecha, int estado, String metodo_pago, double precio) {
+        this.cedula = cedula;
+        this.nombreCompleto = nombreCompleto;
+        this.numOrden = numOrden;
+        this.pizzas = pizzas;
+        this.extras = extras;
+        this.fecha = fecha;
+        this.estado = estado;
+        this.metodo_pago = metodo_pago;
+        this.precio = precio;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public ArrayList<String> getExtras() {
+        return extras;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public String getMetodo_pago() {
+        return metodo_pago;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public int getNumOrden() {
+        return numOrden;
+    }
+
+    public ArrayList<Pizza> getPizzas() {
+        return pizzas;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public void setExtras(ArrayList<String> extras) {
+        this.extras = extras;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setMetodo_pago(String metodo_pago) {
+        this.metodo_pago = metodo_pago;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public void setNumOrden(int numOrden) {
+        this.numOrden = numOrden;
+    }
+
+    public void setPizzas(ArrayList<Pizza> pizzas) {
+        this.pizzas = pizzas;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "orden{" + "cedula=" + cedula + ", nombreCompleto=" + nombreCompleto + ", numOrden=" + numOrden + ", pizzas=" + pizzas.toString() + ", extras=" + extras.toString() + ", fecha=" + fecha + ", estado=" + estado + ", metodo_pago=" + metodo_pago + ", precio=" + precio + '}';
+    }
+}
