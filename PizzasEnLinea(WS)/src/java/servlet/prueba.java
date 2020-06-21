1706009b1e0fedbcc7efc01f3cd0c860f31f9f73
SamantaAlfaro <samantasoul2612@gmail.com>
2020-06-21 15:48:27 -0600

    new pizzaWEBService

--- PizzasEnLinea(WS)/src/java/servlet/prueba.java
+++ PizzasEnLinea(WS)/src/java/servlet/prueba.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package servlets;
+package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Samanta
  */
-public class pruebaServlet extends HttpServlet {
+public class prueba extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -35,10 +35,10 @@ public class pruebaServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet pruebaServlet</title>");            
+            out.println("<title>Servlet prueba</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet pruebaServlet at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet prueba at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
